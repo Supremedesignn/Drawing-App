9ff63942150f315a4837a0c8de5d6d8be8065e21
Nkosi Yafeu <95482668+Supremedesignn@users.noreply.github.com>
2022-05-18 18:16:43 -0400

    Initial Commit

---
+++ Drawing App/Drawing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_AppApp.swift
+//  Drawing App
+//
+//  Created by Nkosi Yafeu on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
